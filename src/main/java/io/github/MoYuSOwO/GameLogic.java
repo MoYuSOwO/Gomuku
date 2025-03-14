bb10685f8e2cd5eef427e9e9813e7a33b645c80c
MoYuSOwO <hhj2006@outlook.com>
2025-03-15 05:15:54 +0800

    run

--- src/main/java/io/github/MoYuSOwO/GameLogic.java
+++ src/main/java/io/github/MoYuSOwO/GameLogic.java
@@ -1,4 +1,152 @@
 package io.github.MoYuSOwO;
 
+import java.util.Arrays;
+
 public class GameLogic {
+    public enum Pieces {
+        EMPTY,
+        WHITE,
+        BLACK;
+    }
+    public enum GameStatus {
+        PLAYING,
+        BLACKWIN,
+        WHITEWIN,
+        DRAW;
+    }
+    private final int boardRow, boardCol;
+    private final Pieces[][] board;
+    private Pieces currentPlayer;
+    private int lastMoveRow, lastMoveCol;
+    public GameLogic(int boardRow, int boardCol) {
+        this.board = new Pieces[boardRow][boardCol];
+        this.boardRow = boardRow;
+        this.boardCol = boardCol;
+        this.lastMoveRow = -1;
+        this.lastMoveCol = -1;
+        for (int i = 0; i < boardRow; i++) {
+            for (int j = 0; j < boardCol; j++) {
+                this.board[i][j] = Pieces.EMPTY;
+            }
+        }
+        this.currentPlayer = Pieces.BLACK;
+    }
+    public void reset() {
+        for (int i = 0; i < boardRow; i++) {
+            Arrays.fill(board[i], Pieces.EMPTY);
+        }
+        currentPlayer = Pieces.BLACK;
+        lastMoveRow = -1;
+        lastMoveCol = -1;
+    }
+    public boolean move(int row, int col) {
+        if (row < 0 || row >= this.boardRow || col < 0 || col >= this.boardCol) {
+            return false;
+        }
+        if (this.board[row][col] == Pieces.EMPTY) {
+            this.board[row][col] = this.currentPlayer;
+            this.lastMoveRow = row;
+            this.lastMoveCol = col;
+            this.changePlayer();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public GameStatus checkResult() {
+        if (this.lastMoveRow == -1 && this.lastMoveCol == -1) {
+            return GameStatus.PLAYING;
+        }
+        else if (this.rowSearch(this.lastMoveRow, this.lastMoveCol) || this.colSearch(this.lastMoveRow, this.lastMoveCol) || this.leftDiagonalSearch(this.lastMoveRow, this.lastMoveCol) || this.rightDiagonalSearch(this.lastMoveRow, this.lastMoveCol)) {
+            if (this.board[this.lastMoveRow][this.lastMoveCol] == Pieces.BLACK) {
+                return GameStatus.BLACKWIN;
+            }
+            else {
+                return GameStatus.WHITEWIN;
+            }
+        }
+        else if (this.isDraw()) {
+            return GameStatus.DRAW;
+        }
+        else {
+            return GameStatus.PLAYING;
+        }
+    }
+    public int getRow() {
+        return this.boardRow;
+    }
+    public int getCol() {
+        return this.boardCol;
+    }
+    public Pieces[][] getBoard() {
+        return Arrays.stream(board).map(Pieces[]::clone).toArray(Pieces[][]::new);
+    }
+    private boolean isDraw() {
+        for (int i = 0; i < this.boardRow; i++) {
+            for (int j = 0; j < this.boardCol; j++) {
+                if (board[i][j] == Pieces.EMPTY) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    private void changePlayer() {
+        if (this.currentPlayer == Pieces.WHITE) {
+            this.currentPlayer = Pieces.BLACK;
+        }
+        else {
+            this.currentPlayer = Pieces.WHITE;
+        }
+    }
+    private boolean rowSearch(int row, int col) {
+        int left = 0;
+        for (int i = col; i >= 0 && this.board[row][i] == this.board[row][col]; i--) {
+            left++;
+        }
+        int right = 0;
+        for (int i = col + 1; i < this.boardCol && this.board[row][i] == this.board[row][col]; i++) {
+            right++;
+        }
+        if (left + right >= 5) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    private boolean colSearch(int row, int col) {
+        int up = 0;
+        for (int i = row; i >= 0 && this.board[i][col] == this.board[row][col]; i--) {
+            up++;
+        }
+        int down = 0;
+        for (int i = row + 1; i < this.boardRow && this.board[i][col] == this.board[row][col]; i++) {
+            down++;
+        }
+        return up + down >= 5;
+    }
+    private boolean leftDiagonalSearch(int row, int col) {
+        int upleft = 0;
+        for (int i = row, j = col; i >= 0 && j >= 0 && this.board[i][j] == this.board[row][col]; i--, j--) {
+            upleft++;
+        }
+        int downright = 0;
+        for (int i = row + 1, j = col + 1; i < this.boardRow && j < this.boardCol && this.board[i][j] == this.board[row][col]; i++, j++) {
+            downright++;
+        }
+        return upleft + downright >= 5;
+    }
+    private boolean rightDiagonalSearch(int row, int col) {
+        int upright = 0;
+        for (int i = row, j = col; i >= 0 && j < this.boardCol && this.board[i][j] == this.board[row][col]; i--, j++) {
+            upright++;
+        }
+        int downleft = 0;
+        for (int i = row + 1, j = col - 1; i < this.boardRow && j >= 0 && this.board[i][j] == this.board[row][col]; i++, j--) {
+            downleft++;
+        }
+        return upright + downleft >= 5;
+    }
 }
--- src/main/java/io/github/MoYuSOwO/Main.java
+++ src/main/java/io/github/MoYuSOwO/Main.java
@@ -1,7 +1,44 @@
 package io.github.MoYuSOwO;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("HaHaHaHa");
+        Canvas canvas = new Canvas(500, 300);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setStroke(Color.RED);   // 设置线条颜色
+        gc.setLineWidth(2);        // 设置线条粗细
+        gc.strokeLine(0, 0, 0, 200); // 从 (0,0) 到 (100,100) 画一条红线
+        Label l = new Label("Hello, JavaFX");
+        Button b = new Button("新游戏");
+        Button b1 = new Button("新游戏1");
+        Button b2 = new Button("新游戏2");
+        Label l1 = new Label("Hello, JavaFX1");
+        Label l2 = new Label("");
+        Label l3 = new Label("Hello, JavaFX3");
+        HBox hb = new HBox(b, b1, b2);
+        Scene scene = new Scene(new BorderPane(canvas, hb, l, l1, l2), 640, 480);
+        stage.setScene(scene);
+        stage.show();
     }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
 }
